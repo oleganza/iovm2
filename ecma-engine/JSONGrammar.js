e0e5541f853ad5545b0eefd9a9338a1ebcd9e13f
Oleg Andreev <oleganza@gmail.com>
2009-05-09 00:21:15 +0200

    added string draft

--- ecma-engine/JSONGrammar.js
+++ ecma-engine/JSONGrammar.js
@@ -1,6 +1,14 @@
 var StringGrammar = function(All, Any, Capture, Char, NotChar, Optional, Y, EOF, Terminator, Before, After)
 {
   var SingleQuotedString = All(Char("'"), Char("abcd"), Char("'"))
-  var DoubleQuotedString = All(Char('"'), Char("abcd"), Char('"'))
+  var DoubleQuotedString = All(Char('"'), Optional(
+    Any(
+      All(
+        "\\",
+        Char("TODO: list of control characters")
+      ),
+      NotChar("\"\\")
+    )
+  ), Char('"'))
   return Any(SingleQuotedString, DoubleQuotedString)
 }
