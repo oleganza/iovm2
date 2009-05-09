3cbcf7739e42091668b28e8f67c6ca2dbf94fc8a
Oleg Andreev <oleganza@gmail.com>
2009-05-09 09:51:49 +0200

    refactored json value

--- ecma-engine/JSONGrammar.js
+++ ecma-engine/JSONGrammar.js
@@ -1,49 +1,57 @@
-var controlCharMap = {
-  "b": "\b",
-  "f": "\f",
-  "n": "\n",
-  "r": "\r",
-  "t": "\t"
-}
+var JSONGrammar = function(All, Any, Capture, Char, NotChar, Optional, Y, EOF, Terminator, Before, After){
 
-var StringGrammar = function(All, Any, Capture, Char, NotChar, Optional, Y, EOF, Terminator, Before, After)
-{  
-  var content = function(quote)
-  {
-    return Y(function(content){
-      var anyChar     = NotChar(quote + "\\")
-      // js accepts anything after backslash
-      var controlChar = NotChar("") // Char("\'\"\\/bfnrt") 
+  var StringGrammar = function()
+  {  
+    var controlCharMap = {
+      "b": "\b",
+      "f": "\f",
+      "n": "\n",
+      "r": "\r",
+      "t": "\t"
+    }
+    
+    var content = function(quote)
+    {
+      return Y(function(content){
+        var anyChar     = NotChar(quote + "\\")
+        // js accepts anything after backslash
+        var controlChar = NotChar("") // Char("\'\"\\/bfnrt") 
 
-      anyChar = Capture(anyChar, function(buf, s){ 
-        return s + buf 
+        anyChar = Capture(anyChar, function(buf, s){ 
+          return s + buf 
+        })
+
+        controlChar = Capture(controlChar, function(buf, s) {
+          return s + (controlCharMap[buf] || buf)
+        })
+
+        var item = Any(
+          All(
+            Char("\\"),
+            controlChar
+          ),
+          anyChar
+        )
+
+        return Any(All(item, content), item)
       })
+    }
 
-      controlChar = Capture(controlChar, function(buf, s) {
-        return s + (controlCharMap[buf] || buf)
-      })
-      
-      var item = Any(
-        All(
-          Char("\\"),
-          controlChar
-        ),
-        anyChar
-      )
-      
-      return Any(All(item, content), item)
-    })
+    var init = function(s){ return "" }
+
+    var SingleQuotedString = Before(All(
+      Char("'"), Optional(content("'")), Char("'")
+    ), init)
+
+    var DoubleQuotedString = Before(All(
+      Char('"'), Optional(content('"')), Char('"')
+    ), init)
+
+    return Any(SingleQuotedString, DoubleQuotedString)
   }
   
-  var init = function(s){ return "" }
-  
-  var SingleQuotedString = Before(All(
-    Char("'"), Optional(content("'")), Char("'")
-  ), init)
-  
-  var DoubleQuotedString = Before(All(
-    Char('"'), Optional(content('"')), Char('"')
-  ), init)
-  
-  return Any(SingleQuotedString, DoubleQuotedString)
+  return Any(StringGrammar())
+    
 }
+
+var StringGrammar = JSONGrammar
