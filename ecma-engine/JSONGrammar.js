2e34031390092d0a6826c95df276806359073022
Oleg Andreev <oleganza@gmail.com>
2009-05-09 09:14:45 +0200

    complete StringGrammar

--- ecma-engine/JSONGrammar.js
+++ ecma-engine/JSONGrammar.js
@@ -8,39 +8,41 @@ var controlCharMap = {
 
 var StringGrammar = function(All, Any, Capture, Char, NotChar, Optional, Y, EOF, Terminator, Before, After)
 {  
-  var content = Y(function(content){
-    var anyChar     = NotChar("\"\\")
-    // js accepts anything after backslash
-    var controlChar = NotChar("") //Char("\'\"\\/bfnrt") 
-    
-    anyChar = Capture(anyChar, function(buf, s){ 
-      return s + buf 
-    })
-    
-    controlChar = Capture(controlChar, function(buf, s) {
-      return s + (controlCharMap[buf] || buf)
-    })
-    
-    return All(
-      Any(
+  var content = function(quote)
+  {
+    return Y(function(content){
+      var anyChar     = NotChar(quote + "\\")
+      // js accepts anything after backslash
+      var controlChar = NotChar("") // Char("\'\"\\/bfnrt") 
+
+      anyChar = Capture(anyChar, function(buf, s){ 
+        return s + buf 
+      })
+
+      controlChar = Capture(controlChar, function(buf, s) {
+        return s + (controlCharMap[buf] || buf)
+      })
+      
+      var item = Any(
         All(
           Char("\\"),
           controlChar
         ),
         anyChar
       )
-      //, Optional(content)
-    )
-  })
+      
+      return Any(All(item, content), item)
+    })
+  }
   
   var init = function(s){ return "" }
   
   var SingleQuotedString = Before(All(
-    Char("'"), Optional(content), Char("'")
+    Char("'"), Optional(content("'")), Char("'")
   ), init)
   
   var DoubleQuotedString = Before(All(
-    Char('"'), Optional(content), Char('"')
+    Char('"'), Optional(content('"')), Char('"')
   ), init)
   
   return Any(SingleQuotedString, DoubleQuotedString)
