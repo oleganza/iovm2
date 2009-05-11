bd2bd5c672a0df582369ad1b01d2229f28a8876f
Oleg Andreev <oleganza@gmail.com>
2009-05-11 19:50:42 +0200

    some upd

--- ecma-engine/IonGrammar.js
+++ ecma-engine/IonGrammar.js
@@ -4,26 +4,35 @@ var IonGrammar = function(All, Any, Capture, Char, NotChar, Optional, Y, EOF, Te
     Base grammar:
       message
       message1 message2
+      message1; message2
       message1[msg1, msg2, ...]
-      id = message
-    
-    Extra:
+      id=[message]
+      
+    Extra (in Ion-based parser):
     - operators
     - brackets
     
   */
   
+  var Plus = function(rule)
+  {
+    return Y(function(seq){
+      return Any(All(rule, seq), rule)
+    })
+  }
   
-  var lineSpace     = Char(" \t")
+  var lineSpace     = Plus(Char(" \t"))
   var space         = Char(" \t\n\r")
   var optLineSpace  = Optional(lineSpace)
   var optSpace      = Optional(space)
   
-  
-  
-  return Y(function(Message){
-    var tail = Any(All(optLineSpace, statementEnd), All())
+  return Y(function(MessageChain){
+    
+    var assignment = All()
+    
+    var solo = Any(assignment, call)
+    var tail = Any(statementTail, nextMessageTail)
     return Any(All(solo, tail), solo)
-  }) 
+  })
     
 }
