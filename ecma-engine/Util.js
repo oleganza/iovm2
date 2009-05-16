ac11a70b94c20a0b8372386c6a09db394a52dbdd
Oleg Andreev <oleganza@gmail.com>
2009-05-16 19:46:52 +0200

    proper inheritance for init()

--- ecma-engine/Util.js
+++ ecma-engine/Util.js
@@ -36,9 +36,57 @@ var O = {
     var C = function(){}
     C.prototype = this
     var obj = new C()
+    
+    if (obj["init"]) obj.init.apply(obj, arguments)
+    
     if (arguments.length < 1) return obj
     var last = arguments[arguments.length - 1]
     if (typeof(last) === "function") last.apply(obj, arguments)
+    
+    var init1 = this.init
+    var init2 = obj.init
+    
+    if (init2 !== init1 || !init2)
+    {
+      obj.init = function(){
+        if (init1) init1.apply(this, arguments)
+        if (init2) init2.apply(this, arguments)
+      }
+    }
+    
     return obj
-  }
+  },
+  init: function(){}
 }
+
+//
+// Test
+//
+
+var Animal = O.create(function(){
+  this.type = "Animal prototype"
+  this.init = function(n){
+    this.type = "New Animal " + n
+  }
+})
+
+var Panda = Animal.create(1, function(){
+  this.init = function(){
+    this.type = this.type.replace(/Animal/, "Panda")
+  }
+})
+
+var Kiwi = Animal.create(2, function(){
+  this.init = function(){
+    this.type = this.type.replace(/Animal/, "Kiwi")
+  }
+})
+
+var Bob = Panda.create(3)
+var Lu  = Kiwi.create(4)
+
+print(Animal["type"])
+print(Panda["type"])
+print(Kiwi["type"])
+print(Bob["type"])
+print(Lu["type"])
